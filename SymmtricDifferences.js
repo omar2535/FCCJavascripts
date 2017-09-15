13a195b0f61e8de6ef42bfe9138d2d5c8aee8fc1
Omar <omar2535@gmail.com>
2017-09-14 23:49:39 -0700

    Completed helper functions

--- SymmtricDifferences.js
+++ SymmtricDifferences.js
@@ -1,9 +1,61 @@
   
-function sym(args) {
+function sym() {
+
+    var newarr = [];
+    var pastarr = [];
+    var translatedarr = initializer(arguments);
+    var length = arguments.length;
+    var purearr = concatall(translatedarr);
+    bubbleSort(purearr);
     
-    return args;
+    console.log(purearr);
+    console.log(arguments[0][1]);
+    console.log(translatedarr);
+
+
+    for(var i=0; i<purearr.length; i++){
+      var currentelement = purearr[i];
+      console.log(currentelement);
+      var counter = 0;
+      //STILL NEED TO COMPLETE WHILE LOOP INCREMENTING I WHEN NOT SAME NUMBER
+      
+    }
+    return newarr;
     
   }
   
-  sym([1, 2, 3], [5, 2, 1, 4]);
+  console.log(sym([1, 2, 3], [5, 2, 1, 4]));
   
+  function initializer(arr){
+    var newarr = [];
+    for(var i=0; i<arr.length; i++){
+      newarr.push(arr[i]);
+    }
+    return newarr;
+  }
+
+function concatall(arrs){
+  var newarr = [];
+    for(var i=0; i<arrs.length; i++){
+      newarr = newarr.concat(arrs[i]);
+    }
+    return newarr;
+}
+
+var a = [33, 103, 3, 726, 200, 984, 198, 764, 9];
+
+function bubbleSort(a)
+{
+    var swapped;
+    do {
+        swapped = false;
+        for (var i=0; i < a.length-1; i++) {
+            if (a[i] > a[i+1]) {
+                var temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
